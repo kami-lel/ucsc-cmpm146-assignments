0f6b4a24ddce46f7bf325939d6e01d9ca00e9e94
kami-lel <77594253+kami-lel@users.noreply.github.com>
2025-02-05 18:27:31 -0800

    on manaul one

--- p4/src/manualHTN.py
+++ p4/src/manualHTN.py
@@ -1,79 +1,148 @@
 import pyhop
 
-'''begin operators'''
+"""begin operators"""
 
-def op_punch_for_wood (state, ID):
-	if state.time[ID] >= 4:
-		state.wood[ID] += 1
-		state.time[ID] -= 4
-		return state
-	return False
 
-def op_craft_wooden_axe_at_bench (state, ID):
-	if state.time[ID] >= 1 and state.bench[ID] >= 1 and state.plank[ID] >= 3 and state.stick[ID] >=2:
-		state.wooden_axe[ID] += 1
-		state.plank[ID] -= 3
-		state.stick[ID] -= 2
-		state.time[ID] -= 1
-		return state
-	return False
+def op_punch_for_wood(state, ID):
+    if state.time[ID] >= 4:
+        state.wood[ID] += 1
+        state.time[ID] -= 4
+        return state
+    return False
 
-# your code here
 
-pyhop.declare_operators (op_punch_for_wood, op_craft_wooden_axe_at_bench)
+def op_craft_wooden_axe_at_bench(state, ID):
+    if (
+        state.time[ID] >= 1
+        and state.bench[ID] >= 1
+        and state.plank[ID] >= 3
+        and state.stick[ID] >= 2
+    ):
+        state.wooden_axe[ID] += 1
+        state.plank[ID] -= 3
+        state.stick[ID] -= 2
+        state.time[ID] -= 1
+        return state
+    return False
 
-'''end operators'''
 
-def check_enough (state, ID, item, num):
-	if getattr(state,item)[ID] >= num: return []
-	return False
+def op_craft_plank(state, ID):
+    if state.time[ID] >= 1 and state.wood[ID] >= 1:
+        state.plank[ID] += 4
+        state.wood[ID] -= 1
+        state.time[ID] -= 1
+        return state
 
-def produce_enough (state, ID, item, num):
-	return [('produce', ID, item), ('have_enough', ID, item, num)]
+    return False
 
-def produce (state, ID, item):
-	if item == 'wood': 
-		return [('produce_wood', ID)]
-	# your code here
-	elif item == 'wooden_axe':
-		# this check to make sure we're not making multiple axes
-		if state.made_wooden_axe[ID] is True:
-			return False
-		else:
-			state.made_wooden_axe[ID] = True
-		return [('produce_wooden_axe', ID)]
-	else:
-		return False
 
-pyhop.declare_methods ('have_enough', check_enough, produce_enough)
-pyhop.declare_methods ('produce', produce)
+def op_craft_stick(state, ID):
+    if state.time[ID] >= 1 and state.plank[ID] >= 2:
+        state.stick[ID] += 4
+        state.plank[ID] -= 2
+        state.time[ID] -= 1
+        return state
 
-'''begin recipe methods'''
+    return False
 
-def punch_for_wood (state, ID):
-	return [('op_punch_for_wood', ID)]
 
-def craft_wooden_axe_at_bench (state, ID):
-	return [('have_enough', ID, 'bench', 1), ('have_enough', ID, 'stick', 2), ('have_enough', ID, 'plank', 3), ('op_craft_wooden_axe_at_bench', ID)]
+def op_wooden_axe_for_wood(state, ID):
+    if state.time[ID] >= 2 and state.wooden_axe[ID] >= 1:
+        state.wood[ID] += 1
+        state.time[ID] -= 2
+        return state
 
-# your code here
+    return False
 
-pyhop.declare_methods ('produce_wood', punch_for_wood)
-pyhop.declare_methods ('produce_wooden_axe', craft_wooden_axe_at_bench)
 
-'''end recipe methods'''
+pyhop.declare_operators(
+    op_wooden_axe_for_wood,
+    op_punch_for_wood,
+    op_craft_wooden_axe_at_bench,
+    op_craft_plank,
+    op_craft_stick,
+)
+
+"""end operators"""
+
+
+def check_enough(state, ID, item, num):
+    if getattr(state, item)[ID] >= num:
+        return []
+    return False
+
+
+def produce_enough(state, ID, item, num):
+    return [("produce", ID, item), ("have_enough", ID, item, num)]
+
+
+def produce(state, ID, item):
+    if item == "wood":
+        return [("produce_wood", ID)]
+
+    elif item == "wooden_axe":
+        # this check to make sure we're not making multiple axes
+        if state.made_wooden_axe[ID] is True:
+            return False
+        else:
+            state.made_wooden_axe[ID] = True
+        return [("produce_wooden_axe", ID)]
+
+    elif item == "stick":
+        return ["op_"]
+
+    else:
+        return False
+
+
+pyhop.declare_methods("have_enough", check_enough, produce_enough)
+pyhop.declare_methods("produce", produce)
+
+"""begin recipe methods"""
+
+
+def punch_for_wood(state, ID):
+    return [("op_punch_for_wood", ID)]
+
+
+def use_wooden_axe_for_wood(state, ID):
+    return [
+        ("have_enough", ID, "wooden_axe", 1),
+        ("op_wooden_axe_for_wood", ID),
+    ]
+
+
+def craft_wooden_axe_at_bench(state, ID):
+    return [
+        ("have_enough", ID, "bench", 1),
+        ("have_enough", ID, "stick", 2),
+        ("have_enough", ID, "plank", 3),
+        ("op_craft_wooden_axe_at_bench", ID),
+    ]
+
+
+pyhop.declare_methods("produce_wood", use_wooden_axe_for_wood, punch_for_wood)
+pyhop.declare_methods("produce_wooden_axe", craft_wooden_axe_at_bench)
+
+"""end recipe methods"""
 
 # declare state
-state = pyhop.State('state')
-state.wood = {'agent': 0}
-state.time = {'agent': 4}
-# state.time = {'agent': 46}
-state.wooden_axe = {'agent': 0}
-state.made_wooden_axe = {'agent': False}
-# your code here 
+state = pyhop.State("state")
+state.wood = {"agent": 0}
+state.bench = {"agent": 0}
+state.time = {"agent": 4}
+# FIXME state.time = {'agent': 46}
+state.wooden_axe = {"agent": 0}
+state.made_wooden_axe = {"agent": False}
 
-# pyhop.print_operators()
-# pyhop.print_methods()
+# TODO your code here
 
-pyhop.pyhop(state, [('have_enough', 'agent', 'wood', 1)], verbose=3)
+pyhop.print_operators()
+pyhop.print_methods()
+
+# pyhop.pyhop(state, [("have_enough", "agent", "wood", 1)], verbose=3)
+pyhop.pyhop(state, [("have_enough", "agent", "wood", 1)], verbose=2)
 # pyhop.pyhop(state, [('have_enough', 'agent', 'wood', 12)], verbose=3)
+
+
+# FIXME pyhop.pyhop(state, [("have_enough", "agent", "wood", 1)], verbose=3)
